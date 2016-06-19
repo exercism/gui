f49031b6c4e9a6c0e65c32803adb8464d16788d4
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-06-19 18:53:16 +0300

    gh-4 Using route actions in tracks
        - To avoid using the controller

--- app/tracks/controller.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  actions: {
-    showTrack(track) {
-      this.transitionToRoute('tracks.track', track);
-    }
-  }
-});
--- app/tracks/route.js
+++ app/tracks/route.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('track');
+  },
+  actions: {
+    showTrack(track) {
+      this.transitionTo('tracks.track', track);
+    }
   }
 });

f285aebdc5142c25cb8fba76af2f7c08652ac499
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-07-11 16:04:52 +0300

    gh-23 Adding icons for package

--- electron.js
+++ electron.js
@@ -32,7 +32,8 @@ app.on('ready', function onReady() {
       minWidth: 1024,
       minHeight: 768,
       width: 1024,
-      height: 768
+      height: 768,
+      icon: 'public/assets/icons/linux.png'
     });
 
     mainWindow.setMenu(null);
